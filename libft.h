ef3ba2c224f9c3175dcde1b52a780d9003053ec6
thk <thk@thk-MacBookPro.local>
2022-03-27 14:43:32 +0900

    modified

--- libft.h
+++ libft.h
@@ -6,10 +6,9 @@
 /*   By: taehooki <taehooki@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/25 00:23:34 by taehooki          #+#    #+#             */
-/*   Updated: 2022/03/25 00:23:36 by taehooki         ###   ########.fr       */
+/*   Updated: 2022/03/26 01:46:24 by taehooki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #ifndef LIBFT_H
 # define LIBFT_H
@@ -17,5 +16,7 @@
 int             ft_isalpha(int c);
 int             ft_isdigit(int c);
 int             ft_isalnum(int c);
+int             ft_isascii(int c);
+int             ft_isprint(int c);
 
 #endif
