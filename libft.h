6477bc425b231dafc63788152e1c800272aeb163
Taehoon <thk@thk-Mac.local>
2022-03-30 14:42:13 +0900

    modified

--- libft.h
+++ libft.h
@@ -13,10 +13,18 @@
 #ifndef LIBFT_H
 # define LIBFT_H
 
-int             ft_isalpha(int c);
-int             ft_isdigit(int c);
-int             ft_isalnum(int c);
-int             ft_isascii(int c);
-int             ft_isprint(int c);
+typedef struct      s_list
+{
+    void            *content;
+    struct s_list   *next;
+}                   t_list;
+
+size_t              ft_strlen(const char *s);
+
+int                 ft_isalpha(int c);
+int                 ft_isdigit(int c);
+int                 ft_isalnum(int c);
+int                 ft_isascii(int c);
+int                 ft_isprint(int c);
 
 #endif
