cc874d1694805b92addec738847f8b270d4e58e8
thk <k2t3h4@gmail.com>
2022-04-01 12:52:43 +0900

    modified

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -14,5 +14,11 @@
 
 char    *ft_strrchr(const char *s, int c)
 {
-
+    size_t len;
+	len = ft_strlen(s);
+	while (len != 0 && s[len] != c)
+		len--;
+	if (s[len] == c)
+		return ((char *)(s + len));
+	return (0);    
 }
