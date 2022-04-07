2d20bdaab510e580cc26802bc8031709e3a0bba5
Taehoon Kim <thk@thk-Mac.local>
2022-04-07 18:46:47 +0900

    modified

--- libft.h
+++ libft.h
@@ -15,13 +15,16 @@
 
 # include <stdlib.h>
 
+// String
 size_t              ft_strlen(const char *s);
-
 char                *ft_strchr(const char *s, int c);
 char                *ft_strrchr(const char *s, int c);
-
 int                 ft_strncmp(const char *s1, const char *s2, size_t n);
 
+// Memory
+void                *ft_memset(void *b, int c, size_t len)
+
+// Etc
 int                 ft_isalpha(int c);
 int                 ft_isdigit(int c);
 int                 ft_isalnum(int c);
