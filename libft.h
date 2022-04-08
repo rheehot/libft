be692d2503986393313e17f89856dd6a162b923c
Taehoon Kim <thk@thk-Mac.local>
2022-04-08 20:10:44 +0900

    modified

--- libft.h
+++ libft.h
@@ -22,7 +22,7 @@ char                *ft_strrchr(const char *s, int c);
 int                 ft_strncmp(const char *s1, const char *s2, size_t n);
 
 // Memory
-void                *ft_memset(void *b, int c, size_t len)
+void                *ft_memset(void *b, int c, size_t len);
 
 // Etc
 int                 ft_isalpha(int c);
