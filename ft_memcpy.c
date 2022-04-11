d64582e8cf97f7fc59673dcca35c23fde78ecb16
Taehoon Kim <thk@thk-Mac.local>
2022-04-11 23:41:32 +0900

    modified

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -16,9 +16,10 @@ void    *ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
 {
     size_t i;
     
-    i = -1;
-    if (dst != src && n)
-        while (++i < n)
-            ((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
-    return (dst);
+	if (dst == NULL && src == NULL)
+		return (0);
+	i = -1;
+	while (++i < n)
+		((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
+	return (dst);
 }
--- libft.h
+++ libft.h
@@ -25,6 +25,7 @@ int                 ft_strncmp(const char *s1, const char *s2, size_t n);
 void                *ft_memset(void *b, int c, size_t len);
 void                ft_bzero(void *s, size_t n);
 void                *ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
+void                *ft_memmove(void *dst, const void *src, size_t len);
 
 // Etc
 int                 ft_isalpha(int c);
