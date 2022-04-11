1ffd666b086139890f9c5188850b230fa2cb4f04
Taehoon Kim <thk@thk-Mac.local>
2022-04-11 15:05:59 +0900

    modified

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -10,4 +10,15 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+void    *ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
+{
+    size_t i;
+    
+    i = -1;
+    if (dst != src && n)
+        while (++i < n)
+            ((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
+    return (dst);
+}
--- libft.h
+++ libft.h
@@ -24,6 +24,7 @@ int                 ft_strncmp(const char *s1, const char *s2, size_t n);
 // Memory
 void                *ft_memset(void *b, int c, size_t len);
 void                ft_bzero(void *s, size_t n);
+void                *ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
 
 // Etc
 int                 ft_isalpha(int c);
