43137c73b3410ec7b75df6dded660e07f910a237
Taehoon Kim <thk@thk-Mac.local>
2022-04-18 19:31:52 +0900

    modified

--- ft_atoi.c
+++ ft_atoi.c
@@ -13,3 +13,26 @@
 #include "libft.h"
 
 int ft_atoi(const char *str)
+{
+    int i;
+    int sign;
+    int res;
+
+    i   = 0;
+    sign = 1;
+    res = 0;
+	while (str[i] == ' ' || ('\t' <= str[i] && str[i] <= '\r'))
+        ++i;
+    if (str[i] == '+' || str[i] == '-')
+    {
+        if (str[i] == '-')
+            sign = -1;
+        i++;
+    }
+	while (ft_isdigit(str[i]))
+	{
+		res = (res * 10) + (str[i] - '0');
+		i++;
+	}
+	return (res * sign);
+}
