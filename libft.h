0a67ea08643cc3b16e231d775094bcd56f2f8e4a
Taehoon Kim <thk@thk-Mac.local>
2022-04-24 15:57:44 +0900

    modified

--- libft.h
+++ libft.h
@@ -26,6 +26,8 @@ int                 ft_strncmp(const char *s1, const char *s2, size_t n);
 char                *ft_strdup(const char *s1);
 char                *ft_substr(char const *s, unsigned int start, size_t len);
 char                *ft_strjoin(char const *s1, char const *s2);
+char				*ft_strtrim(char const *s1, char const *set);
+char				**ft_split(char const *s, char c);
 
 // Memory
 void                *ft_memset(void *b, int c, size_t len);
