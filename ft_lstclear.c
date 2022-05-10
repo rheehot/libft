6d541d03cfaf8b3d2f60d3bc34049ae6fd27aaf3
Taehoon Kim <taehooki@c4r4s3.42seoul.kr>
2022-05-10 22:03:11 +0900

    modified

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -16,11 +16,13 @@ void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
 	t_list	*tmp;
 
+	if (!lst || !del)
+		return ;
 	while (*lst)
 	{
 		tmp = (*lst)->next;
 		ft_lstdelone(*lst, del);
 		(*lst) = tmp;
 	}
-	*lst = (void *)0;
+	*lst = 0;
 }
--- ft_lstmap.c
+++ ft_lstmap.c
@@ -17,18 +17,21 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list	*new;
 	t_list	*tmp;
 
-	new = NULL;
+	new = 0;
+	tmp = 0;
 	while (lst)
 	{
+		if (!f || !del || !lst)
+			return (0);
 		tmp = ft_lstnew(f(lst->content));
 		if (!tmp)
 		{
 			ft_lstclear(&new, del);
-			return ((void *)(0));
+			return (0);
 		}
 		ft_lstadd_back(&new, tmp);
 		lst = lst->next;
 	}
-	tmp = NULL;
+	tmp = 0;
 	return (new);
 }
