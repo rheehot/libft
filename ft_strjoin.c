d82e10362889942368adbf4ba5dd7be99d1ac410
Taehoon Kim <thk@thk-Mac.local>
2022-06-03 21:55:25 +0900

    change

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: taehooki <taehooki@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/25 01:17:57 by taehooki          #+#    #+#             */
-/*   Updated: 2022/05/09 15:41:57 by taehooki         ###   ########.fr       */
+/*   Updated: 2022/06/03 21:55:04 by taehooki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,12 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	char	*tmp;
 
 	if (!s1 || !s2)
-		return (0);
+		return (NULL);
 	len_s1 = ft_strlen(s1);
 	len_s2 = ft_strlen(s2);
 	tmp = (char *)malloc(len_s1 + len_s2 + 1);
 	if (!(tmp))
-		return (0);
+		return (NULL);
 	ft_memcpy(tmp, s1, len_s1);
 	ft_memcpy(tmp + len_s1, s2, len_s2);
 	tmp[len_s1 + len_s2] = '\0';
