68f7501f1271240cdef86b94c09dd717584c5dfd
Taehoon Kim <thk@thk-Mac.local>
2022-06-09 20:50:49 +0900

    change

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: taehooki <taehooki@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/25 00:33:07 by taehooki          #+#    #+#             */
-/*   Updated: 2022/05/09 15:35:56 by taehooki         ###   ########.fr       */
+/*   Updated: 2022/06/09 20:45:53 by taehooki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,8 @@ void	*ft_calloc(size_t count, size_t size)
 	if (size && (count > SIZE_MAX / size))
 		return (malloc(SIZE_MAX));
 	ptr = malloc(count * size);
-	if (ptr)
-		ft_bzero(ptr, count * size);
+	if (!ptr)
+		return (NULL);
+	ft_bzero(ptr, count * size);
 	return (ptr);
 }
